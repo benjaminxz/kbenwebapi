3fb3485a94fd2eea4b59351121ac816f148cb5d6
benjamin <novabarry>
2016-11-28 17:50:50 +0800

    Kbw-6 删除list中的数据 动态删除前台的数据

--- api/Object/Controllers/objectController.cs
+++ api/Object/Controllers/objectController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using api.Object.DTO;
 using CMS.CustomTables;
+using CMS.DataEngine;
 
 namespace api.Object.Controllers
 {
@@ -13,9 +14,6 @@ namespace api.Object.Controllers
     {
         public List<apiCrud> Get()
         {
-
-            //string name = CustomTableItemProvider.GetItems("bjm.apiCrud").FirstOrDefault().GetStringValue("apiName", "");
-            //return name;
             List<apiCrud> stringList = new List<apiCrud>();//这里必须这样声明
             var TableItems = CustomTableItemProvider.GetItems("bjm.apiCrud");
             foreach (var item in TableItems)
@@ -29,41 +27,54 @@ namespace api.Object.Controllers
                 }
             }
             return stringList;
-
         }
 
         public string Get(int id)
         {
             string name = CustomTableItemProvider.GetItems("bjm.apiCrud").FirstOrDefault().GetStringValue("apiName", "");
             return name;
-            //List<string> stringList = new List<string>();//这里必须这样声明
-            //var TableItems = CustomTableItemProvider.GetItems("bjm.apiCrud");
-            //foreach (var item in TableItems)
-            //{
-            //    if (item.GetStringValue("apiName", "") != null)
-            //    {
-            //        stringList.Add(item.GetStringValue("apiName", ""));
-            //    }
-            //}
-            //return stringList;
         }
 
-        [HttpPost]
-        public string SaveData(dynamic obj)
+        [HttpDelete]
+        public string DeleteObject(int id)
         {
-            string strName = Convert.ToString(obj.NAME);
-            if (!string.IsNullOrEmpty(strName))
-            {
-                var newApiCrudItem = new CustomTableItem("bjm.apiCrud");
-                newApiCrudItem.SetValue("apiName", strName);
-                newApiCrudItem.Insert();
-                return "save " + strName + " success";
-            }
-            else
-            {
-                return "not success:you insert a null value.";
-            }
+            string customTableClassName = "bjm.apiCrud";
 
+            // Gets the custom table
+            DataClassInfo customTable = DataClassInfoProvider.GetDataClassInfo(customTableClassName);
+            if (customTable != null)
+            {
+                // Gets the first custom table record whose value in the 'ItemText' starts with 'New text'
+                CustomTableItem item = CustomTableItemProvider.GetItems(customTableClassName)
+                                                                    .WhereEquals("ItemID", id)
+                                                                    .FirstObject;
+
+                if (item != null)
+                {
+                    // Deletes the custom table record from the database
+                    item.Delete();
+                    return "delete success";
+                }
+            }
+            return "delete flase";
         }
+
+        //[HttpPost]
+        //public string SaveData(dynamic obj)
+        //{
+        //    string strName = Convert.ToString(obj.NAME);
+        //    if (!string.IsNullOrEmpty(strName))
+        //    {
+        //        var newApiCrudItem = new CustomTableItem("bjm.apiCrud");
+        //        newApiCrudItem.SetValue("apiName", strName);
+        //        newApiCrudItem.Insert();
+        //        return "save " + strName + " success";
+        //    }
+        //    else
+        //    {
+        //        return "not success:you insert a null value.";
+        //    }
+
+        //}
     }
 }
