cb1931a3d912a1adddbec6dc581b43d3fc8ce7c2
benjamin <novabarry>
2016-11-30 14:49:38 +0800

    Kbw-5 图片的显示

---
+++ api/Object/Controllers/PictureController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using CMS.CustomTables;
+using CMS.Helpers;
+
+namespace api.Object.Controllers
+{
+    public class PictureController: ApiController
+    {
+        public string Get()
+        {
+
+            string name = CustomTableItemProvider.GetItems("bjm.apiBusiness").FirstObject.GetStringValue("Picture", "");
+
+            return URLHelper.GetAbsoluteUrl(name);
+        }
+    }
+}
