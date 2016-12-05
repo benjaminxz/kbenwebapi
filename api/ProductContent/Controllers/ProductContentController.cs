73e94d8e4a9b6636e0a619288f210521272eaf8a
benjamin <novabarry>
2016-12-05 17:24:30 +0800

    Kbw-15 类电商的效果实现-菜品详情

---
+++ api/ProductContent/Controllers/ProductContentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using api.ProductContent.DTO;
+using CMS.Base;
+using CMS.CustomTables;
+using CMS.Helpers;
+
+namespace api.Content.Controllers
+{
+    public class ProductContentController : ApiController
+    {
+        public List<apiProductContent> Get(string productGuid)
+        {
+            Guid proGuid = new Guid(productGuid);//convert string to Guid
+            List<apiProductContent> stringList = new List<apiProductContent>();//这里必须这样声明
+            var TableItems = CustomTableItemProvider.GetItems("bjm.apiProductContent").WhereEquals("ProductGuid", proGuid).OrderBy("OrderNumber");//DESC 
+            if (TableItems.HasResults())
+            {
+                foreach (var item in TableItems)
+                {
+
+                    var apiProductItem = new apiProductContent
+                    {
+                        OrderNumber = item.GetIntegerValue("OrderNumber", 0),
+                        IsPicture = item.GetBooleanValue("IsPicture", false),
+                        ContentTextd = item.GetStringValue("ContentText", ""),
+                        ContentPicture = URLHelper.GetAbsoluteUrl(item.GetStringValue("ContentPicture", ""))
+                    };
+                    stringList.Add(apiProductItem);
+
+                }
+
+            }
+            return stringList;
+        }
+    }
+}
---
+++ api/ProductContent/DTO/apiProductContent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.ProductContent.DTO
+{
+    public class apiProductContent
+    {
+        public int OrderNumber { get; set; }
+        public bool IsPicture { get; set; }
+        public string ContentTextd { get; set; }
+        public string ContentPicture { get; set; }
+    }
+}
