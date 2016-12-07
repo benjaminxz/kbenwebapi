e77ca80b79e1ad717a19cbdc5584d848e4f29e2a
benjamin <novabarry>
2016-12-07 18:09:44 +0800

    Kbw-22 商家搜索与进入 -1  id ok

---
+++ api/ProductList/Controllers/ProductListController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using api.ProductList.DTO;
+using CMS.CustomTables;
+
+namespace api.ProductList.Controllers
+{
+    public class ProductListController : ApiController
+    {
+        public List<apiProduct> Get(int id)
+        {
+            List<apiProduct> stringList = new List<apiProduct>();//这里必须这样声明
+            var TableItems = CustomTableItemProvider.GetItems("bjm.apiProduct").WhereEquals("BusinessId", id).OrderBy("OrderNumber");//DESC 
+            if (TableItems.HasResults())
+            {
+                foreach (var item in TableItems)
+                {
+
+                    var apiProductItem = new apiProduct
+                    {
+                        OrderNumber = item.GetIntegerValue("OrderNumber", 0),
+                        ProductName = item.GetStringValue("ProductName", ""),
+                    };
+                    stringList.Add(apiProductItem);
+
+                }
+
+            }
+            return stringList;
+        }
+    }
+}
---
+++ api/ProductList/DTO/apiProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.ProductList.DTO
+{
+    public class apiProduct
+    {
+        public string ProductName { get; set; }
+        public int OrderNumber { get; set; }
+    }
+}
