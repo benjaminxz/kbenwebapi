53003740b09791f66983b6da433e8b1fe7e530f9
benjamin <novabarry>
2016-12-08 11:23:42 +0800

    Kbw-22 商家搜索与进入

---
+++ api/BusinessSearch/Controllers/BusinessSearchController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using api.BusinessSearch.DTO;
+using CMS.CustomTables;
+
+namespace api.BusinessSearch.Controllers
+{
+    public class BusinessSearchController:ApiController
+    {
+        public List<apiBusiness> Get(string searchBusinessName)
+        {
+            List<apiBusiness> stringList = new List<apiBusiness>();//这里必须这样声明
+            var TableItems = CustomTableItemProvider.GetItems("bjm.apiBusiness").WhereContains("BusinessName", searchBusinessName);//DESC WhereLike("BusinessName", searchBusinessName)
+            if (TableItems.HasResults())
+            {
+                foreach (var item in TableItems)
+                {
+
+                    var apiBusinessItem = new apiBusiness
+                    {
+                        ItemId = item.GetIntegerValue("ItemId", 0),
+                        BusinessName = item.GetStringValue("BusinessName", ""),
+                        BusinessPicture = item.GetStringValue("BusinessPicture", ""),
+                    };
+                    stringList.Add(apiBusinessItem);
+
+                }
+
+            }
+            return stringList;
+        }
+    }
+}
---
+++ api/BusinessSearch/DTO/apiBusiness.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.BusinessSearch.DTO
+{
+    public class apiBusiness
+    {
+        public int ItemId { get; set; }
+        public string BusinessName { get; set; }
+        public string BusinessPicture { get; set; }
+
+    }
+}
